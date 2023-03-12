d5da808d8ffe31f57ac721212ce9c38f0ac4c924
Junior Paula <luke.junnior@icloud.com>
2023-03-12 20:04:14 -0300

    Connect mysql and get messages from kafka

---
+++ cmd/app/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"go-message-kafka/internal/infra/repositories"
+	"go-message-kafka/internal/infra/repositories/akafka"
+	"go-message-kafka/internal/usecases"
+
+	"github.com/confluentinc/confluent-kafka-go/kafka"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(host.docker.internal:3306/products)")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	msgChan := make(chan *kafka.Message)
+	go akafka.Consume([]string{"products"}, "host.docker.internal:9094", msgChan)
+
+	repository := repositories.NewProductRepositoryMysql(db)
+	createProductUsecase := usecases.NewCreateProductUsecase(repository)
+
+	for msg := range msgChan {
+		dto := usecases.CreateProductInputDto{}
+		err := json.Unmarshal(msg.Value, &dto)
+		if err != nil {
+			fmt.Println("error to get messages from kafka")
+			continue
+		}
+		_, err = createProductUsecase.Execute(dto)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
