4232f939a80906b99750658761e0be7db71f28ff
Junior Paula <luke.junnior@icloud.com>
2023-03-12 20:17:56 -0300

    Create product handlers from http request

---
+++ internal/infra/web/product_handlers.go
@@ -0,0 +1,36 @@
+package web
+
+import (
+	"encoding/json"
+	"go-message-kafka/internal/usecases"
+	"net/http"
+)
+
+type ProductHanlders struct {
+	CreateProductUsecase *usecases.CreateProductUsecase
+	ListProductsUsecase  *usecases.ListProductsUsecase
+}
+
+func NewProductHandlers(createProductUsecase *usecases.CreateProductUsecase, listProductsUsecase *usecases.ListProductsUsecase) *ProductHanlders {
+	return &ProductHanlders{
+		CreateProductUsecase: createProductUsecase,
+		ListProductsUsecase:  listProductsUsecase,
+	}
+}
+
+func (p *ProductHanlders) CreateProductHanlder(w http.ResponseWriter, r *http.Request) {
+	var input usecases.CreateProductInputDto
+	err := json.NewDecoder(r.Body).Decode(&input)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+	output, err := p.CreateProductUsecase.Execute(input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(output)
+}
